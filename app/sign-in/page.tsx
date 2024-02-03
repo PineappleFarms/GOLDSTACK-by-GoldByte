3249f63f0fc82e555fa32a4ad629bb48b37d549c
Overload02 <alvajoy123@gmail.com>
2024-02-03 17:43:21 -0500

    'test'

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -2,46 +2,39 @@ import { auth } from '@/auth'
 import { LoginButton } from '@/components/login-button'
 import { animate, px } from 'framer-motion'
 import { redirect } from 'next/navigation'
-// import anime from 'animejs';
-
-const quotes = [
-  "Gold will not be scarce, and anyone who possesses it will be no more noble than the one who possesses dirt.",
-  "Knowledge is wealth; ignorance is poverty.",
-  "Learning is an ornament to wealth, but poverty without it is shameful.",
-];
-
 
 export default async function SignInPage() {
   const session = await auth()
+
   // redirect to home if user is already logged in
   if (session?.user) {
     redirect('/')
   }
 
   return (
-    <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center " style={{
-      backgroundImage: `url('https://i.imgur.com/or5KQbE.jpeg')`,
+    <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center" style={{
+      backgroundImage: `url('https://i.imgur.com/HyKdfEY.png')`,
       backgroundSize: 'cover', // Fill the background
       backgroundPosition: 'center',
-      flexWrap: 'wrap',
+      flexWrap: 'wrap'
     }}>
 
 
       <div>
-        <p id="quote" style={{ fontSize: '35px', color: 'white' }}>
-          “Education is the greatest wealth a man can possess.” - Mansa Musa
+        <p id="quote" style={{ fontSize: '35px', color: 'black' }}>
+          <b>
+            <center>
+              “Education is the greatest wealth a man can possess.” <br /> - Mansa Musa
+            </center>
+          </b>
         </p>
       </div>
-      
-      <div style={{width: '100%'}}>
 
-      </div>
+      <div style={{ width: '100%' }}></div>
+
       <div>
         <LoginButton />
       </div>
-      
-
-
 
     </div>
   )
